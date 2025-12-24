8903b08d06835d6fae5a9c7ac65a71c57ec7678f
sahilduttpb06 <sahilduttpb06@gmail.com>
2025-12-25 10:19:07 +1030

    error

--- assets/js/chatscript.js
+++ assets/js/chatscript.js
@@ -1,10 +1,10 @@
-const typebotInitScript = document.createElement("script");
-typebotInitScript.type = "module";
-typebotInitScript.innerHTML = `import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0/dist/web.js'
+// const typebotInitScript = document.createElement("script");
+// typebotInitScript.type = "module";
+// typebotInitScript.innerHTML = `import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0/dist/web.js'
 
-Typebot.initBubble({
-  typebot: "lead-generation-ol73zwa",
-  theme: { button: { backgroundColor: "#149ddd" } },
-});
-`;
-document.body.append(typebotInitScript);
+// Typebot.initBubble({
+//   typebot: "lead-generation-ol73zwa",
+//   theme: { button: { backgroundColor: "#149ddd" } },
+// });
+// `;
+// document.body.append(typebotInitScript);
